53a25153d7a0b8c6189b28636197f09d48a3029a
kengz <kengzwl@gmail.com>
2025-01-11 23:03:20 -0500

    feat: rebuild with pydantic
    BREAKING CHANGE: rebuild with pydantic

--- test/fixture/transform.py
+++
@@ -1,9 +0,0 @@
-from sklearn import datasets
-import pandas as pd
-import pytest
-
-
-@pytest.fixture(scope='session')
-def test_data_df():
-    # columns: sepal length (cm), sepal width (cm), petal length (cm), petal width (cm), target
-    return pd.concat(datasets.load_iris(return_X_y=True, as_frame=True), axis=1)
